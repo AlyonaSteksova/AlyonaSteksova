b66997de470489fbf2917c54be97e40b8ac22a17
AlyonaSteksova <98020792+AlyonaSteksova@users.noreply.github.com>
2023-07-14 15:55:45 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rnd = new Random();
+Console.WriteLine("Введите начальную дату: ");
+DateTime min=Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Введите конечную дату: ");
+DateTime max = Convert.ToDateTime(Console.ReadLine());
+int a = 1;
+
+for (DateTime i=min; i<max; i=i.AddHours(1))
+{
+    int value = rnd.Next(-15, 15);
+    Console.WriteLine(a + "  |  " + i + "  |  " + value);
+    a++;
+}
